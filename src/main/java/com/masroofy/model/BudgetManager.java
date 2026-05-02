ed377731d0ea975e118721c3b4af8c1e2a7bec65
nada <20242370@stud.fci-cu.edu.eg>
2026-05-02 20:31:09 +0300

    Add and edit methods based on class & sequence diagrams

--- src/main/java/com/masroofy/model/BudgetManager.java
+++ src/main/java/com/masroofy/model/BudgetManager.java
@@ -1,31 +1,46 @@
-package com.masroofy.service;
+package com.masroofy.model;
 
 import com.masroofy.dao.SQLiteDatabase;
+import com.masroofy.service.NotificationService;
+import com.masroofy.service.TransactionManager;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 // Service Layer
 public class BudgetManager {
 
     SQLiteDatabase db = SQLiteDatabase.getInstance();
+    private TransactionManager transactionManager ;
+    private NotificationService notificationService;
+    
 
-    // Sequence: calculateDailyLimit
-    public double calculateDailyLimit(double allowance, int days) {
+    public long calculateDaysBetween(String firstDate, String lastDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate startDate = LocalDate.parse(firstDate, formatter);
+        LocalDate endDate = LocalDate.parse(lastDate, formatter);
+
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+        return daysBetween;
+    }
+
+    // Sequence: calculateSafeDailyLimit
+    public double calculateSafeDailyLimit(double allowance, long days) {
         if (days == 0) return 0;
         return allowance / days;
     }
 
-    // Sequence: Create Budget Cycle
-    public void createBudget(double allowance, String start, String end, int days) {
-        double daily = calculateDailyLimit(allowance, days);
-        db.saveBudget(allowance, start, end, daily);
+    // Sequence: Create Budget
+    public void createBudget(double allowance, String start, String end) {
+        long days =calculateDaysBetween(start,end);
+        double dailyLimit = calculateSafeDailyLimit(allowance, days);
+        db.saveBudget(allowance, start, end, dailyLimit);
     }
 
-    // Sequence: calculateSafeDailyLimit
-    public double calculateSafeDailyLimit(double balance, int days) {
-        if (days == 0) return 0;
-        return balance / days;
-    }
-
-    // Sequence: get Daily Limit
+    // Sequence: get safe Daily Limit
     public double getSafeDailyLimit(double allowance, String endDate) {
         double balance = db.getRemainingBalance(allowance);
         int days = db.getRemainingDays(endDate);
@@ -35,5 +50,24 @@ public class BudgetManager {
         db.saveSafeDailyLimit(safe);
 
         return safe;
+    }
+    
+    public double reCalculateSafeDailyLimit(int userID) {
+        int days= db.getRemainingDays(userID);
+        double balance = db.getRemainingBalance(userID);
+        double newDailyLimit = calculateSafeDailyLimit(balance, days);
+        db.saveSafeDailyLimit(balance);
+        return newDailyLimit ;
+    }
+    
+    public String calculatePercentage(int userID, double totalBudget){
+        double totalSpent= db.getTotalExpenses(userID);
+        return (totalSpent/totalBudget)*100 +"%";
+    }
+    
+    public void resetCycle(int userID){
+        transactionManager.deleteTransaction(userID);
+        notificationService.showNotification("Rest Done Successfuly");
+        
     }
 }
