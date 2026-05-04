f80d89298b8b84f537be3dac3c2d50b985d8313f
nada <20242370@stud.fci-cu.edu.eg>
2026-05-04 22:36:14 +0300

    Rename the package (service -> view)

---
+++ src/main/java/com/masroofy/view/NotificationService.java
@@ -0,0 +1,16 @@
+package com.masroofy.view;
+
+public class NotificationService {
+    public void showNotification(String message) {
+        System.out.println("Notification: " + message);
+    }
+
+    public boolean showConfirmationMessage(String message) {
+        System.out.println("Confirmation: " + message);
+       return true;
+    }
+
+    public void showError(String message) {
+        System.err.println("Error: " + message);
+    }
+}
